1100cb6bec1062ee4a202216424292ebe44ca119
Aron Bordin <aron.bordin@gmail.com>
2015-04-02 13:34:52 -0300

    Simple tutorial about camera on Android

---
+++ SimpleCamera/app/src/main/java/com/wordpress/bytedebugger/simplecamera/CameraView.java
@@ -0,0 +1,65 @@
+package com.wordpress.bytedebugger.simplecamera;
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.io.IOException;
+
+public class CameraView extends SurfaceView implements SurfaceHolder.Callback{
+    private SurfaceHolder mHolder;
+    private Camera mCamera;
+
+    public CameraView(Context context, Camera camera){
+        super(context);
+
+        mCamera = camera;
+        mCamera.setDisplayOrientation(90);
+        //get the holder and set this class as the callback, so we can get camera data here
+        mHolder = getHolder();
+        mHolder.addCallback(this);
+        mHolder.setType(SurfaceHolder.SURFACE_TYPE_NORMAL);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+        try{
+            //when the surface is created, we can set the camera to draw images in this surfaceholder
+            mCamera.setPreviewDisplay(surfaceHolder);
+            mCamera.startPreview();
+        } catch (IOException e) {
+            Log.d("ERROR", "Camera error on surfaceCreated " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i2, int i3) {
+        //before changing the application orientation, you need to stop the preview, rotate and then start it again
+        if(mHolder.getSurface() == null)//check if the surface is ready to receive camera data
+            return;
+
+        try{
+            mCamera.stopPreview();
+        } catch (Exception e){
+            //this will happen when you are trying the camera if it's not running
+        }
+
+        //now, recreate the camera preview
+        try{
+            mCamera.setPreviewDisplay(mHolder);
+            mCamera.startPreview();
+        } catch (IOException e) {
+            Log.d("ERROR", "Camera error on surfaceChanged " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+        //our app has only one screen, so we'll destroy the camera in the surface
+        //if you are unsing with more screens, please move this code your activity
+        mCamera.stopPreview();
+        mCamera.release();
+    }
+}
---
+++ SimpleCamera/app/src/main/java/com/wordpress/bytedebugger/simplecamera/MainActivity.java
@@ -0,0 +1,44 @@
+package com.wordpress.bytedebugger.simplecamera;
+
+import android.app.Activity;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
+
+
+public class MainActivity extends Activity {
+    private Camera mCamera = null;
+    private CameraView mCameraView = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        try{
+            mCamera = Camera.open();//you can use open(int) to use different cameras
+        } catch (Exception e){
+            Log.d("ERROR", "Failed to get camera: " + e.getMessage());
+        }
+
+        if(mCamera != null) {
+            mCameraView = new CameraView(this, mCamera);//create a SurfaceView to show camera data
+            FrameLayout camera_view = (FrameLayout)findViewById(R.id.camera_view);
+            camera_view.addView(mCameraView);//add the SurfaceView to the layout
+        }
+
+        //btn to close the application
+        ImageButton imgClose = (ImageButton)findViewById(R.id.imgClose);
+        imgClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.exit(0);
+            }
+        });
+    }
+}
